081c1b37326cad347fe8c779931e51a16a9f5f2c
Zoen-DEV <enzoholgadodev@gmail.com>
2024-12-16 12:20:53 -0500

    [auth]: login long screen and responsive implemented

---
+++ src/utils/hooks/use_screen_width.ts
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export default function useScreenWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+    return () =>
+      window.removeEventListener("resize", () => setWidth(window.innerWidth));
+  }, []);
+
+  return width;
+}
