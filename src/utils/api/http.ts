e4ecedb74f70bb29a2adb07af0ba50a8f98f3b1f
Enzo Holgado <enzoholgadodev@gmail.com>
2025-04-10 12:16:08 -0500

    [api]: comment temporary http interceptors

--- src/utils/api/http.ts
+++ src/utils/api/http.ts
@@ -14,42 +14,42 @@ const http = axios.create({
 //   },
 // });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const interceptors = (config: any) => {
-  const stringTokens = localStorage.getItem("tokens");
-  const expiresAt = localStorage.getItem("expiresAt");
-  const now = new Date();
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// const interceptors = (config: any) => {
+//   const stringTokens = localStorage.getItem("tokens");
+//   const expiresAt = localStorage.getItem("expiresAt");
+//   const now = new Date();
 
-  if (expiresAt && now.getTime() > parseInt(expiresAt)) {
-    localStorage.removeItem("tokens");
-    localStorage.removeItem("user");
-    localStorage.removeItem("expiresAt");
-    window.location.href = "/";
-  }
+//   if (expiresAt && now.getTime() > parseInt(expiresAt)) {
+//     localStorage.removeItem("tokens");
+//     localStorage.removeItem("user");
+//     localStorage.removeItem("expiresAt");
+//     window.location.href = "/";
+//   }
 
-  let token = null;
+//   let token = null;
 
-  if (stringTokens) {
-    const tokens = JSON.parse(stringTokens);
+//   if (stringTokens) {
+//     const tokens = JSON.parse(stringTokens);
 
-    if (tokens.expiresAt) {
-      const expiresAt = new Date(tokens.expiresAt);
+//     if (tokens.expiresAt) {
+//       const expiresAt = new Date(tokens.expiresAt);
 
-      if (expiresAt.getTime() < now.getTime()) {
-        localStorage.removeItem("tokens");
-        localStorage.removeItem("user");
-        window.location.href = "/";
-      }
-    }
+//       if (expiresAt.getTime() < now.getTime()) {
+//         localStorage.removeItem("tokens");
+//         localStorage.removeItem("user");
+//         window.location.href = "/";
+//       }
+//     }
 
-    token = tokens.accessToken;
-  }
+//     token = tokens.accessToken;
+//   }
 
-  config.headers.Authorization = `Bearer ${token}`;
-  return config;
-};
+//   config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// };
 
-http.interceptors.request.use(interceptors);
+// http.interceptors.request.use(interceptors);
 // httpFormData.interceptors.request.use(interceptors);
 
 export default http;
